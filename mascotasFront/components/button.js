845b9a3e408abaa1da7b60388c3c900a4109704c
Oscar Penilla <140924351+WaffleAtomico@users.noreply.github.com>
2025-10-29 16:41:22 -0600

    Append ListOfElements
    Se agrego la lista de elementos
    Cambios en pruebas.js para mas claridad de objetos para tarjetas
    Cambios en input para validaciones
    Cambios en button para el tipo

--- mascotasFront/components/button.js
+++ mascotasFront/components/button.js
@@ -1,17 +1,16 @@
-// Función que crea un botón completamente editable con Bootstrap
 export function createButton({ 
     text = "Botón", 
     color = "primary", 
     size = "md", 
+    type = "button", //Puede ser submit
     icon = "", 
     outlined = false 
 } = {}) {
     const btnType = outlined ? `btn-outline-${color}` : `btn-${color}`;
     const btnSize = size === "sm" || size === "lg" ? `btn-${size}` : "";
     
-    // Creamos el innerHTML directamente como variable lista para insertar
     const buttonHTML = `
-        <button type="button" class="btn ${btnType} ${btnSize} d-flex align-items-center justify-content-center gap-2">
+        <button type="${type}" class="btn ${btnType} ${btnSize} d-flex align-items-center justify-content-center mx-auto d-block gap-2">
             ${icon ? `<i class="${icon}"></i>` : ""}
             <span>${text}</span>
         </button>
