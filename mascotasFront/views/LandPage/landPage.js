d423dc2d5e33abc4aca04dbe6f07a447ad4db1c1
Oscar Penilla <140924351+WaffleAtomico@users.noreply.github.com>
2025-10-29 18:49:42 -0600

    Modificacion Organizacion
    Modifique la organizacion de las paginas
    Deje las paginas pendientes, falta agregar diseño en varias paginas
    Modifique el boton,acepta iconos a derecha e izquierda
    La barra de navegacion tiene los iconos correspondientes

---
+++ mascotasFront/views/LandPage/landPage.js
@@ -0,0 +1,16 @@
+import { createButton } from "../../components/button.js"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginButtonContainer = document.getElementById("login-button");
+    const loginButton = createButton({text: "Acceder", 
+                                            iconright: "bi bi-box-arrow-in-right", 
+                                            color:"primary"})
+    loginButtonContainer.innerHTML = loginButton;
+    
+    
+    const createAccButtonContainer = document.getElementById("createAcc-button");
+    const createAccButton = createButton({text: "Crear cuenta", 
+                                            iconleft: "bi bi-person-fill-add", 
+                                            color:"success"})
+    createAccButtonContainer.innerHTML = createAccButton;
+})
